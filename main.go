0440d849b629104c6d06d12341cc2cc8009ad446
morzik <morzik45@gmail.com>
2020-10-30 16:14:02 +0500

    add devops

--- main.go
+++ main.go
@@ -4,6 +4,11 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"os"
+	"time"
 )
 
 // Структура запроса (см. https://cloud.yandex.ru/docs/functions/concepts/function-invoke#request)
@@ -15,12 +20,19 @@ type RequestBody struct {
 
 // Преобразуем поле body объекта RequestBody
 type Request struct {
-	Name string `json:"name"`
+	Name  string    `json:"name"`
+	Start time.Time `json:"start"`
+	End   time.Time `json:"end"`
 }
 
 type Response struct {
 	StatusCode int         `json:"statusCode"`
 	Body       interface{} `json:"body"`
+}
+
+type Data struct {
+	Id    string `bson:"_id" json:"id"`
+	Count int    `bson:"count" json:"count"`
 }
 
 func Handler(ctx context.Context, request []byte) (*Response, error) {
@@ -41,11 +53,39 @@ func Handler(ctx context.Context, request []byte) (*Response, error) {
 		return nil, fmt.Errorf("an error has occurred when parsing body: %v", err)
 	}
 
-	name := req.Name
+	if req.Name == "new_users" {
+		data, _ := GetData(*req)
+		return &Response{
+			StatusCode: 200,
+			Body:       data,
+		}, nil
+	}
 	// Тело ответа необходимо вернуть в виде структуры, которая автоматически преобразуется в JSON-документ,
 	// который отобразится на экране
 	return &Response{
 		StatusCode: 200,
-		Body:       fmt.Sprintf("Hello, %s", name),
+		Body:       "",
 	}, nil
+}
+
+func GetData(req Request) ([]Data, error) {
+	var ctx = context.TODO()
+	var data []Data
+	clientOptions := options.Client().ApplyURI(os.Getenv("MONGO_URI"))
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		return data, err
+	}
+	if err := client.Ping(ctx, nil); err != nil {
+		return data, err
+	}
+	Users := client.Database(os.Getenv("DB_NAME")).Collection("users")
+
+	matchStage := bson.D{{"$match", bson.D{{"datetimes.first_visit", bson.D{{"$lt", time.Now()}, {"$gt", time.Now().Add(-time.Hour * 144)}}}}}}
+	groupStage := bson.D{{"$group", bson.D{{"_id", bson.D{{"$dateToString", bson.D{{"format", "%Y-%m-%d"}, {"date", "$datetimes.first_visit"}}}}}, {"count", bson.D{{"$sum", 1}}}}}}
+	sortStage := bson.D{{"$sort", bson.D{{"_id", 1}}}}
+
+	result, err := Users.Aggregate(ctx, mongo.Pipeline{matchStage, groupStage, sortStage})
+	err = result.All(ctx, &data)
+	return data, err
 }
